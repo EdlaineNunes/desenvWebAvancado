1701c73667e1f611b870ac45aeb25d21014e14da
Edlaine Nunes <edlaine.nunes@b2wdigital.com>
2021-07-18 02:14:33 -0300

    feat: initial ajustments pages html and framework

--- Atividade_1/project_web/atividadeweb/webapp/urls.py
+++ Atividade_1/project_web/atividadeweb/webapp/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
-from .views import index
+from .views import index, w3c, html, css, javascript, frontend, backend
 
 urlpatterns = [
-    path('', index),
+    path('home/', index),
+    path('w3c/', w3c, name="w3c"),
+    path('html', html, name="html"),
+    path('css/', css, name="css"),
+    path('javascript/', javascript, name="javascript"),
+    path('frontend/', frontend, name="frontend"),
+    path('backend/', backend, name="backend"),
+
+
 ]
--- Atividade_1/project_web/atividadeweb/webapp/views.py
+++ Atividade_1/project_web/atividadeweb/webapp/views.py
@@ -2,8 +2,34 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
-    context = {
-        'curso': 'Sistemas de Informação'
-    }
-    return render(request, 'index.html', context)
+    # context = {
+    #     'curso': 'Sistemas de Informação'
+    # }
+    # return render(request, 'index.html', context)
+    return render(request, 'index.html')
+
+
+def w3c(request):
+    return render(request, 'w3c.html')
+
+
+def html(request):
+    return render(request, 'html.html')
+
+
+def css(request):
+    return render(request, 'css.html')
+
+
+def javascript(request):
+    return render(request, 'javascript.html')
+
+
+def frontend(request):
+    return render(request, 'frontend.html')
+
+
+def backend(request):
+    return render(request, 'backend.html')
