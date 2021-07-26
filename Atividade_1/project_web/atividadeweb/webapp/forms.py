64a583e768c0c62899eeb70273a3086a7d391c44
Edlaine Nunes <edlaine.nunes@b2wdigital.com>
2021-07-26 20:21:56 -0300

    refactor: ajustements page contacts and views && urls && forms

--- Atividade_1/project_web/atividadeweb/webapp/forms.py
+++ Atividade_1/project_web/atividadeweb/webapp/forms.py
@@ -1,8 +1,26 @@
 from django import forms
+from django.core.mail import message
+from django.core.mail.message import EmailMessage
 
 class ContactForm(forms.Form):
     name = forms.CharField(label='Nome', max_length=100)
     emailAddress = forms.EmailField(label='Email', max_length=100)
     subjectMatter = forms.CharField(label='Assunto', max_length=100)
     message = forms.CharField(label='Mensagem', widget=forms.Textarea())
+
+    def send_mail(self):
+        name = self.cleaned_data['name']
+        emailAddress = self.cleaned_data['emailAddress']
+        subjectMatter = self.cleaned_data['subjectMatter']
+        message = self.cleaned_data['message']
+
+        contents = f'Nome: {name}\nE-mail: {emailAddress}\nAssunto: {subjectMatter}\nMensagem: {message}'
+        mail = EmailMessage (
+            subject=subjectMatter,
+            body=contents,
+            from_email='contato@seudominio.com.br',
+            to=['contato@seudominio.com.br', ],
+            headers={'Reply.To':emailAddress},
+        )
+        mail.send()
 
--- Atividade_1/project_web/atividadeweb/webapp/views.py
+++ Atividade_1/project_web/atividadeweb/webapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import * 
+from django.contrib import messages
 
 # Create your views here.
 
@@ -36,7 +37,17 @@ def backend(request):
     return render(request, 'backend.html')
 
 def contact(request):
-    form = ContactForm()
+    form = ContactForm(request.POST or None)
+    
+    if str(request.method) == 'POST':
+        if form.is_valid():
+            form.send_mail()
+            messages.success(request, 'E-mail enviado com sucesso')
+            form = ContactForm()
+
+        else:
+            messages.error(request,'Erro ao enviar e-mail')
+
     context = {
         'form': form
     }
